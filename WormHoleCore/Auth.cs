1f5170f6f8d6472fa59f5b12aa657c1907669410
fang.su@foxmail.com <fang.su@foxmail.com>
2015-09-17 16:01:45 +0800

    sorry

--- WormHoleCore/Auth.cs
+++ WormHoleCore/Auth.cs
@@ -4,10 +4,8 @@ namespace WormHoleCore
 {
 	public static class Auth
 	{
-		static bool loggedIn = false;
-
-		public static bool LoggedIn {
-			get { return loggedIn; } 
+		static Auth ()
+		{
 		}
 	}
 }
