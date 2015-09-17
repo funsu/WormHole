e6480f0a0c8abc7d50ccc9e2815dd6946de5b787
fang.su@foxmail.com <fang.su@foxmail.com>
2015-09-17 16:08:58 +0800

    sorry

--- WormHoleCore/Auth.cs
+++ WormHoleCore/Auth.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace WormHoleCore
 {
 	public static class Auth
 	{
+		public static bool LoggedIn = false;
+		public static string serverPubKey;
+		public static string clientPrivateKey;
 		static Auth ()
 		{
 		}
+
 	}
 }
 
