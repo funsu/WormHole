8c43b58beafa4641759ed7403afa7c00a8507363
fang.su@foxmail.com <fang.su@foxmail.com>
2015-09-18 15:56:14 +0800

    change serializer

--- WormHoleCore/HeaderSerializer.cs
+++ WormHoleCore/HeaderSerializer.cs
@@ -5,9 +5,11 @@ using Newtonsoft.Json;
 using System.Runtime.Remoting.Messaging;
 using System.IO;
 
+
+//需要重写
 namespace WormHoleCore
 {
-	public static class HttpSerializer
+	public static class HeaderSerializer
 	{
 		static HashSet<int> relayIdSet = new HashSet<int> ();
 		static Random randomInt = new Random ();
@@ -26,52 +28,50 @@ namespace WormHoleCore
 
 		public static string serializeRequest (HttpListenerRequest request)
 		{
-			var httpRequest = new HttpRequest ();
-			httpRequest.rid = getRid ();
-			httpRequest.method = request.HttpMethod;
-			httpRequest.url = request.RawUrl;
+			var headerRequest = new HeaderRequest ();
+//			httpRequest.rid = getRid ();
+			headerRequest.method = request.HttpMethod;
+			headerRequest.url = request.RawUrl;
 			var headers = new Dictionary<string, string> ();
 			foreach (var h in request.Headers.AllKeys) {
 				if (HeaderUtil.isEndToEndHeader (h))
 					headers.Add (h, request.Headers [h]);
 			}
-			httpRequest.headers = headers;
+			headerRequest.headers = headers;
 			if (request.HasEntityBody) {
 				var body = new MemoryStream ();
 				request.InputStream.CopyTo (body);
-				httpRequest.body = body.ToArray ();
+				headerRequest.body = body.ToArray ();
 			}
-			return JsonConvert.SerializeObject (httpRequest);
+			return JsonConvert.SerializeObject (headerRequest);
 		}
 
 		public static void deserializeResponse (string response, HttpListenerResponse originalResponse)
 		{
-			var httpResponse = JsonConvert.DeserializeObject<HttpRespons> (response);
-			if (relayIdSet.Contains (httpResponse.rid))
-				relayIdSet.Remove (httpResponse.rid);
-			originalResponse.StatusCode = httpResponse.statusCode;
-			foreach (var h in httpResponse.headers) {
+			var headerResponse = JsonConvert.DeserializeObject<HeaderRespons> (response);
+			if (relayIdSet.Contains (headerResponse.rid))
+				relayIdSet.Remove (headerResponse.rid);
+			originalResponse.StatusCode = headerResponse.statusCode;
+			foreach (var h in headerResponse.headers) {
 				if (HeaderUtil.isEndToEndHeader (h.Key))
 					originalResponse.AddHeader (h.Key, h.Value);
 			}
-			originalResponse.OutputStream.Write (httpResponse.body, 0, httpResponse.body.Length);
+			//originalResponse.OutputStream.Write (headerResponse.body, 0, headerResponse.body.Length);
 		}
 
-		class HttpRequest
+		class HeaderRequest
 		{
-			public int rid;
+//			public int rid;
 			public string method;
 			public string url;
 			public Dictionary<string, string> headers;
-			public byte[] body;
+//			public byte[] body;
 		}
 
-		class HttpRespons
+		class HeaderRespons
 		{
-			public int rid = 0;
 			public int statusCode = 0;
 			public Dictionary<string, string> headers = null;
-			public byte[] body = null;
 		}
 	}
 }
